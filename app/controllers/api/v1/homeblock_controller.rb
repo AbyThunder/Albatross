e708dfd91cf4e371d28788b986770b78188439f1
Abhiraj Singh <abhiraj.singh@toptal.com>
2023-12-15 03:35:45 +0100

    Fix

--- app/controllers/api/v1/homeblock_controller.rb
+++ app/controllers/api/v1/homeblock_controller.rb
@@ -1,6 +1,6 @@
 module Api
   module V1
-    class HomeblocksController < ApplicationController
+    class HomeblockController < ApplicationController
       before_action :authenticate_user!
       load_and_authorize_resource
 
