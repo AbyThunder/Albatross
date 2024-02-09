c2c49451776930c39767db978ea06aa22600380c
ritwick malhotra <malhotraritwick2011@gmail.com>
2024-02-09 23:11:09 +0100

    feat(authorization): fixing specs

--- app/serializers/lesson_serializer.rb
+++ app/serializers/lesson_serializer.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
-class LessonSerializer
-  include FastJsonapi::ObjectSerializer
+class LessonSerializer < ActiveModel::Serializer
   attributes :date, :place, :description, :freebie
 
   belongs_to :academy
