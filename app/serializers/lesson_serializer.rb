de5a5c674f7bce3652c8b98d515ca991c82a7bb6
Abhiraj Singh <abhiraj.singh@toptal.com>
2024-03-05 17:37:17 +0100

    add lesson rewards

--- app/serializers/lesson_serializer.rb
+++ app/serializers/lesson_serializer.rb
@@ -3,5 +3,6 @@
 class LessonSerializer < ActiveModel::Serializer
   attributes :date, :place, :description, :freebie, :name, :time, :status, :id
 
+  has_many :lesson_rewards
   belongs_to :academy
 end
