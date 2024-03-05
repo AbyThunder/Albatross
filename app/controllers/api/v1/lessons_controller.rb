6903d46800c61bdaf7a4976c86a49a529c42db22
Abhiraj Singh <abhiraj.singh@toptal.com>
2024-03-05 17:51:15 +0100

    FIx lesson

--- app/controllers/api/v1/lessons_controller.rb
+++ app/controllers/api/v1/lessons_controller.rb
@@ -33,7 +33,7 @@ module Api
         lesson = Lesson.find(params[:id])
 
         if lesson.update(permitted_params)
-          create_or_update_rewards(lesson, params, update: true)
+          create_or_update_rewards(lesson, params[:rewards], update: true)
           render json: { message: 'Lesson updated successfully' }, status: :ok
         else
           render json: { errors: lesson.errors.full_messages }, status: :unprocessable_entity
