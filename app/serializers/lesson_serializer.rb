333d9ff0e857a2b2110df1bd379f6d5ee359e692
Abhiraj Singh <abhiraj.singh@toptal.com>
2024-03-05 16:56:52 +0100

    Add id to lesson

--- app/serializers/lesson_serializer.rb
+++ app/serializers/lesson_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class LessonSerializer < ActiveModel::Serializer
-  attributes :date, :place, :description, :freebie, :name, :time, :status
+  attributes :date, :place, :description, :freebie, :name, :time, :status, :id
 
   belongs_to :academy
 end
